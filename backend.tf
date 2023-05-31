ea304ecab2866645d246462ee5c03624dac6f031
Rafael Martinez <rgmllc@yahoo.com>
2023-05-31 22:37:50 +0000

    Updated code

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "ThisismyWeek21Bucket"  # Replace with your desired S3 bucket name
-    key    = "example.tfstate"  # Replace with your desired state file name
-    region = "us-east-1"        # Replace with your desired region
+    bucket = "my-terraform-backend-bucket-luit" 
+    key    = "terraform.tfstate"
+    region = "us-west-1" 
   }
 }
